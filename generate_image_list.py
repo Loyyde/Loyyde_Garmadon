1f32d877932cb576bce4d66bc87484abff195db5
Loyyde <36266172+Loyyde@users.noreply.github.com>
2025-03-31 10:52:21 +0000

    Update generate_image_list.py

--- generate_image_list.py
+++ generate_image_list.py
@@ -7,13 +7,10 @@ image_files = []
 if os.path.exists(slide_folder) and os.path.isdir(slide_folder):
     for filename in os.listdir(slide_folder):
         if any(filename.lower().endswith(ext) for ext in image_extensions):
-            image_files.append(filename)
+            # Prepend the folder name to the filename
+            image_files.append(os.path.join(slide_folder, filename))
 
-javascript_array = "const imageFilenames = [\n"
-if image_files:
-    javascript_array += "    '" + "',\n    '".join(image_files) + "'\n];"
-else:
-    javascript_array += "];"
+javascript_array = f"const imageFilenames = [\n    '{''},\n    '.join(image_files)}'\n];"
 
 output_filename = 'image_list.js'
 with open(output_filename, 'w') as f:
