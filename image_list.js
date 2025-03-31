3cad077049b4deb9b0eed98e70f2d315a9d82f53
Loyyde <Loyyde@users.noreply.github.com>
2025-03-31 12:46:21 +0000

    Update image lists

--- image_list.js
+++ image_list.js
@@ -1,7 +1,14 @@
 const imageFilenames = [
-    'Slide/WhatsApp Image 2025-03-26 at 19.56.12_286ecbe5.jpg',
-    'Slide/WhatsApp Image 2025-03-26 at 19.56.17_74aab698.jpg',
-    'Slide/WhatsApp Image 2025-03-26 at 19.56.15_8f45ca7a.jpg',
-    'Slide/WhatsApp Image 2025-03-26 at 19.56.16_1969ce5d.jpg',
-    'Slide/WhatsApp Image 2025-03-26 at 19.56.12_6e55832d.jpg'
+    'Showcase/Kendrick/image_2025-03-31_122615516.png',
+    'Showcase/Kendrick/image_2025-03-31_122528470.png',
+    'Showcase/Kendrick/image_2025-03-31_122541589.png',
+    'Showcase/Kendrick/image_2025-03-31_122600685.png',
+    'Showcase/Kendrick/image_2025-03-31_122629127.png',
+    'Showcase/Kendrick/image_2025-03-31_122641700.png',
+    'Showcase/Drake/download (4).jpeg',
+    'Showcase/Drake/download.jpeg',
+    'Showcase/Drake/download (5).jpeg',
+    'Showcase/Drake/download (1).jpeg',
+    'Showcase/Drake/download (3).jpeg',
+    'Showcase/Drake/download (2).jpeg'
 ];
---
+++ slide_list.js
@@ -0,0 +1,7 @@
+const slideImageFilenames = [
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.12_286ecbe5.jpg',
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.17_74aab698.jpg',
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.15_8f45ca7a.jpg',
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.16_1969ce5d.jpg',
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.12_6e55832d.jpg'
+];
