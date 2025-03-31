84924d0fd4813cc2b6f6e3105768cc3ef6690fc5
Loyyde <Loyyde@users.noreply.github.com>
2025-03-31 10:55:56 +0000

    Update image list

--- image_list.js
+++ image_list.js
@@ -1,7 +1,3 @@
 const imageFilenames = [
-    'WhatsApp Image 2025-03-26 at 19.56.12_286ecbe5.jpg',
-    'WhatsApp Image 2025-03-26 at 19.56.17_74aab698.jpg',
-    'WhatsApp Image 2025-03-26 at 19.56.15_8f45ca7a.jpg',
-    'WhatsApp Image 2025-03-26 at 19.56.16_1969ce5d.jpg',
-    'WhatsApp Image 2025-03-26 at 19.56.12_6e55832d.jpg'
+    'Slide/WhatsApp Image 2025-03-26 at 19.56.12_286ecbe5.jpg', 'Slide/WhatsApp Image 2025-03-26 at 19.56.17_74aab698.jpg', 'Slide/WhatsApp Image 2025-03-26 at 19.56.15_8f45ca7a.jpg', 'Slide/WhatsApp Image 2025-03-26 at 19.56.16_1969ce5d.jpg', 'Slide/WhatsApp Image 2025-03-26 at 19.56.12_6e55832d.jpg'
 ];
