18bd73381a6d5d7505c6e0d1a43b89b01f2a6fd6
Loyyde <Loyyde@users.noreply.github.com>
2025-03-31 10:31:14 +0000

    Update image list

--- image_list.js
+++ image_list.js
@@ -1,4 +1,7 @@
 const imageFilenames = [
-    '',
-    ''.join(image_files)
+    'WhatsApp Image 2025-03-26 at 19.56.12_286ecbe5.jpg',
+    'WhatsApp Image 2025-03-26 at 19.56.17_74aab698.jpg',
+    'WhatsApp Image 2025-03-26 at 19.56.15_8f45ca7a.jpg',
+    'WhatsApp Image 2025-03-26 at 19.56.16_1969ce5d.jpg',
+    'WhatsApp Image 2025-03-26 at 19.56.12_6e55832d.jpg'
 ];
